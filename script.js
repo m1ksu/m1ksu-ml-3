16b369184c8c29bd9a00bb3f53f57dc6dd74ffc2
m1ksu <mikael.stenman112@gmail.com>
2017-02-02 07:19:12 +0200

    commited whole project directory

---
+++ script.js
@@ -0,0 +1,32 @@
+$(function() {
+
+    $(document).on('click','#w1', function(){
+        $('html, body').animate({
+            scrollTop: $(".wp1").offset().top,
+        }, {duration: 600, easing: 'easeInOutSine'});
+    });
+
+    $(document).on('click','#w2', function(){
+        $('html, body').animate({
+            scrollTop: $(".wp2").offset().top,
+        }, {duration: 600, easing: 'easeInOutSine'});
+    });
+
+    $(document).on('click','#w3', function(){
+        $('html, body').animate({
+            scrollTop: $(".wp3").offset().top,
+        }, {duration: 600, easing: 'easeInOutSine'});
+    });
+
+
+    $(document).on('click', 'body', function(){
+        $('navTable').animate({opacity: 1});
+    });
+
+    window.onscroll = function () {
+
+        if ($(window).scrollTop() >= ($(".wp1").position().top - 100)) {
+            $('.navTable').animate({opacity: 1});
+        }
+    }
+});
